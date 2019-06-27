d3564df48af7cde28e9c821c8cd1508231e5f62d
Kavon Farvardin <kavon@farvard.in>
2019-06-27 11:38:02 -0500

    improve the simple test

--- test/simple.cpp
+++ test/simple.cpp
@@ -1,12 +1,40 @@
+//////
+// This test is designed such that it exhibits non-fixed code hotness.
+// The first few iterations are only calls to fib, each of which take take
+// a constant amount of time.
+//
+// Then, we begin to run a hailstone sequence calculation, and each call to that
+// takes progressively more time (by a linear amount).
+//
+// Given enough iterations, the hailstone sequence will dominate the running
+// time both in aggregate and snapshot.
 
-void runBench(long start, long limit) {
-  // 27 is a good starting number
-  long x = start;
+#define NO_INLINE __attribute__((noinline))
+
+// knobs to control workload
+#define NUM_FIB_ONLY 4
+#define ITERS 10
+
+// https://oeis.org/A006577/list
+#define START_HAILSTONE 27
+
+// pick a number such that ITERS * fib(START_FIB) doesn't overflow unsigned long
+#define START_FIB 40
+
+NO_INLINE unsigned long fib(unsigned long n) {
+  if (n < 2)
+    return n;
+
+  return fib(n-1) + fib(n-2);
+}
+
+NO_INLINE void compute_hailstone(long limit) {
+  long x = START_HAILSTONE;
   long reachedOne = 0;
 
   while (reachedOne < limit) {
     if (x == 1) {
-      x = start;
+      x = START_HAILSTONE;
       reachedOne++;
     }
 
@@ -19,7 +47,17 @@ void runBench(long start, long limit) {
 
 int main() //(int argc, const char **argv)
 {
-    runBench(27, 5'000'000);
+    unsigned long ans = 0;
+    int i;
 
-    return 0;
+    for (i = -NUM_FIB_ONLY; i < ITERS; i++) {
+      compute_hailstone(500'000 * i);
+      ans += fib(START_FIB);
+    }
+
+    if (ans % i == 0) {
+      return 0;
+    }
+
+    return 1;
 }
