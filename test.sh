19dac3e46d2180921eb6f376af524e7614d0e0d2
Kavon Farvardin <kavon@farvard.in>
2019-08-09 17:45:14 -0500

    comment out benchmark run in testing for now

--- test.sh
+++ test.sh
@@ -11,12 +11,12 @@ cmake --build . -- test-halo
 popd
 
 ##### run benchmark suite
-if [ "${KIND}" == "docker" ]; then
-  git submodule sync benchmark
-  git submodule update --init benchmark
-fi
-
+# if [ "${KIND}" == "docker" ]; then
+#   git submodule sync benchmark
+#   git submodule update --init benchmark
+# fi
 # ./benchmark.sh
+
 ## we could then check the results of the benchmark for perf regressions
 
 if [ "${KIND}" == "docker" ]; then
