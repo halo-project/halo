d03162542816761fbadcb838bb4f8f07406812ec
Kavon Farvardin <kavon@farvard.in>
2019-08-11 20:52:29 -0500

    adding another regression test

--- test/bench/fftbench.cpp
+++ test/bench/fftbench.cpp
@@ -1,3 +1,17 @@
+// RUN: %clang -DSMALL_PROBLEM_SIZE -fhalo -O1 %s -o %t
+// RUN: %testhalo %server 1 %t %t-out.txt
+// RUN: cat %t-out.txt | FileCheck %s.expected
+
+
+//---------------------------------------------------------------------------
+// number of loops to perform
+#ifdef SMALL_PROBLEM_SIZE
+static const int TEST_SIZE   =  2097152;
+#else
+static const int TEST_SIZE   =  4*2097152;
+#endif
+
+
 /*
     fftbench
     Standard C++ version
@@ -12,7 +26,7 @@
     A simplified version of the FFT component from my standard libcoyote
     library, this benchmark test FFT performance on complex<double> values.
 
-    Note that the code herein is design for the purpose of testing 
+    Note that the code herein is design for the purpose of testing
     computational performance; error handling and other such "niceties"
     is virtually non-existent.
 
@@ -35,22 +49,6 @@
 using namespace std;
 
 //---------------------------------------------------------------------------
-// number of loops to perform
-#if defined(ACOVEA)
-#if defined(arch_pentium4)
-static const int TEST_SIZE   =   262144;
-#else
-static const int TEST_SIZE   =   131072;
-#endif
-#else
-#ifdef SMALL_PROBLEM_SIZE
-static const int TEST_SIZE   =  50000;
-#else
-static const int TEST_SIZE   =  2097152/4;
-#endif
-#endif
-
-//---------------------------------------------------------------------------
 // embedded random number generator; ala Park and Miller
 static double random_double()
 {
@@ -64,17 +62,17 @@ static double random_double()
 
     long k;
     double result;
-    
+
     seed ^= MASK;
     k = seed / IQ;
     seed = IA * (seed - k * IQ) - IR * k;
-    
+
     if (seed < 0)
         seed += IM;
-    
+
     result = AM * seed;
     seed ^= MASK;
-    
+
     return result;
 }
 
@@ -85,50 +83,50 @@ class polynomial
     public:
         // creation constructor, uninitialized (coefficients will be unknown values)
         polynomial(size_t degree);
-    
+
         // creation constructor, with initialization from array
         polynomial(size_t degree, const T * coefficients);
-    
+
         // creation constructor, with single-value initialization constructor
         polynomial(size_t degree, const T & value);
-    
+
         // copy constructor
         polynomial(const polynomial<T> & source);
-        
+
         // destructor
         virtual ~polynomial();
-        
+
         // assignment
         polynomial<T> & operator = (const polynomial<T> & source);
-        
+
         // initialize to specific value
         void initialize(const T & value = T(0));
-    
+
         // increase polynomial length
         polynomial<T> & stretch(size_t degrees);
-        
+
         // interrogate for degree
         size_t degree() const
         {
             return m_degree;
         }
-        
+
         // get specific coefficient
         T get(size_t term) const;
-        
+
         T & operator [] (size_t term);
-        
+
         // evaluate for a specific value
         T operator() (const T & x) const;
-        
+
         // unitary operators
         polynomial<T> operator - () const;
         polynomial<T> operator + () const;
-        
+
         // binary mathematical operators
         polynomial<T> operator + (const polynomial<T> & poly) const;
         polynomial<T> operator - (const polynomial<T> & poly) const;
-        
+
         // constant required by FFT routines
         static const complex<T> PI2I;
 
@@ -153,25 +151,35 @@ class polynomial
 
         // multiplication via FFT
         polynomial<T> operator * (const polynomial<T> & poly) const;
-        
+
         // shorthand mathematical operators
         polynomial<T> & operator += (const polynomial<T> & poly);
         polynomial<T> & operator -= (const polynomial<T> & poly);
         polynomial<T> & operator *= (const polynomial<T> & poly);
-        
+
+        void dump(ostream& os, size_t limit) {
+          // NOTE: not sure if it's a polynomial of one variable.
+          // Just want some output for regression tests.
+          limit = min(degree(), limit);
+          for (size_t i = 0; i < limit; i++) {
+            os << this->get(i) << " * X^" << i <<" +\n";
+          }
+          os << "0\n";
+        }
+
     protected:
         // coefficients
         T * m_coeff;
-    
+
         // number of terms
         size_t m_degree;
-        
+
         // acquire resources
         void acquire();
-        
+
         // release resources
         void release();
-        
+
         // deep copy
         void deep_copy(const T * source);
 };
@@ -205,7 +213,7 @@ inline void polynomial<T>::initialize(const T & value)
     for (size_t n = 0; n < m_degree; ++n)
         m_coeff[n] = value;
 }
-    
+
 // creation constructor, uninitialized (coefficients will be unknown values)
 template <typename T>
 polynomial<T>::polynomial(size_t degree)
@@ -222,7 +230,7 @@ polynomial<T>::polynomial(size_t degree, const T * coefficients)
     m_degree(degree)
 {
     acquire();
-    
+
     if (coefficients != NULL)
         deep_copy(coefficients);
 }
@@ -236,7 +244,7 @@ polynomial<T>::polynomial(size_t degree, const T & value)
     acquire();
     initialize(value);
 }
-    
+
 // copy constructor
 template <typename T>
 polynomial<T>::polynomial(const polynomial<T> & source)
@@ -261,13 +269,13 @@ polynomial<T> & polynomial<T>::operator = (const polynomial<T> & source)
     if (m_degree != source.m_degree)
     {
         release();
-    
+
         m_degree = source.m_degree;
         acquire();
     }
-    
+
     deep_copy(source.m_coeff);
-    
+
     return *this;
 }
 
@@ -279,19 +287,19 @@ polynomial<T> & polynomial<T>::stretch(size_t degrees)
     {
         T * old_coeff = m_coeff;
         size_t old_degree = m_degree;
-    
+
         m_degree += degrees;
         acquire();
-        
+
         size_t n = 0;
-        
+
         for (; n < old_degree; ++n)
             m_coeff[n] = old_coeff[n];
-        
+
         for (; n < m_degree; ++n)
             m_coeff[n] = T(0);
     }
-    
+
     return *this;
 }
 
@@ -314,19 +322,19 @@ T polynomial<T>::operator() (const T & x) const
 {
     // using Horner's Rule
     T y = m_coeff[m_degree - 1];
-    
+
     size_t i = m_degree - 2;
-    
+
     while (true)
     {
         y = x * y + m_coeff[i];
-        
+
         if (i > 0)
             --i;
         else
             break;
     }
-    
+
     return y;
 }
 
@@ -335,17 +343,17 @@ template <typename T>
 polynomial<T> polynomial<T>::operator - () const
 {
     polynomial<T> result(m_degree); // uninitialized
-    
+
     for (size_t n = 0; n < m_degree; ++n)
         result.m_coeff[n] = -m_coeff[n];
-    
+
     return result;
 }
 
 template <typename T>
 polynomial<T> polynomial<T>::operator + () const
 {
-    return polynomial<T>(*this); 
+    return polynomial<T>(*this);
 }
 
 // binary mathematical operators
@@ -354,20 +362,20 @@ polynomial<T> polynomial<T>::operator + (const polynomial<T> & poly) const
 {
     if (m_degree >= poly.m_degree)
     {
-        polynomial<T> result(*this); 
-    
+        polynomial<T> result(*this);
+
         for (size_t n = 0; n < m_degree; ++n)
             result.m_coeff[n] += poly.m_coeff[n];
-    
+
         return result;
     }
     else
     {
-        polynomial<T> result(poly); 
-    
+        polynomial<T> result(poly);
+
         for (size_t n = 0; n < m_degree; ++n)
             result.m_coeff[n] += m_coeff[n];
-    
+
         return result;
     }
 }
@@ -377,20 +385,20 @@ polynomial<T> polynomial<T>::operator - (const polynomial<T> & poly) const
 {
     if (m_degree >= poly.m_degree)
     {
-        polynomial<T> result(*this); 
-    
+        polynomial<T> result(*this);
+
         for (size_t n = 0; n < m_degree; ++n)
             result.m_coeff[n] -= poly.m_coeff[n];
-    
+
         return result;
     }
     else
     {
-        polynomial<T> result(poly); 
-    
+        polynomial<T> result(poly);
+
         for (size_t n = 0; n < m_degree; ++n)
             result.m_coeff[n] -= m_coeff[n];
-    
+
         return result;
     }
 }
@@ -483,7 +491,7 @@ template <typename T>
 polynomial< complex<T> > polynomial<T>::bit_reverse(const polynomial< complex<T> > & poly)
 {
     size_t b = log2(poly.degree());
-    
+
     polynomial< complex<T> > result(poly.degree());
 
     for (size_t n = 0; n < poly.degree(); ++n)
@@ -499,7 +507,7 @@ polynomial< complex<T> > polynomial<T>::fft(const polynomial<T> & poly)
     size_t nl = log2(poly.degree());
     size_t j, k, m, m2, s;
     complex<T> wm, w, t, u;
-    
+
     polynomial< complex<T> > result = bit_reverse(poly);
 
     m  = 2;
@@ -537,7 +545,7 @@ polynomial< complex<T> > polynomial<T>::inverse_fft(const polynomial< complex<T>
     size_t nl = log2(poly.degree());
     size_t j, k, m, m2, s;
     complex<T> wm, w, t, u;
-    
+
     polynomial< complex<T> > result = bit_reverse(poly);
 
     m  = 2;
@@ -579,9 +587,9 @@ polynomial<T> polynomial<T>::operator * (const polynomial<T> & poly) const
     // brute force algorithm
     if (m_degree != poly.m_degree)
         throw domain_error("can not multiply two polynomials with different lengths");
-    
+
     polynomial<T> result(2 * m_degree - 1, T(0));
-    
+
     for (size_t n1 = 0; n1 < m_degree; ++n1)
         for (size_t n2 = 0; n2 < m_degree; ++n2)
             result.m_coeff[n1 + n2] += m_coeff[n1] * poly.m_coeff[n2];
@@ -589,20 +597,20 @@ polynomial<T> polynomial<T>::operator * (const polynomial<T> & poly) const
     // duplicate p1 and p2 to preserve original objects
     polynomial<T> a1(*this);
     polynomial<T> a2(poly);
-    
+
     // expand polynomials to next-largest power of two
     if (a1.degree() > a2.degree())
         a2.stretch(a1.stretch_fft());
     else
         a1.stretch(a2.stretch_fft());
-    
+
     // FFT polynomials
     polynomial< complex<T> > dft1 = fft(a1);
     polynomial< complex<T> > dft2 = fft(a2);
 
     // multiply coefficients
     size_t n2 = a1.degree();
-    
+
     for (size_t k = 0; k < n2; ++k)
         dft1[k] *= dft2[k];
 
@@ -616,7 +624,7 @@ polynomial<T> polynomial<T>::operator * (const polynomial<T> & poly) const
     for (size_t k = 0; k < n2; ++k)
         result[k] = dft2[k].real();
 #endif
-    
+
     return result;
 }
 
@@ -625,10 +633,10 @@ template <typename T>
 polynomial<T> & polynomial<T>::operator += (const polynomial<T> & poly)
 {
     size_t length = (m_degree <= poly.m_degree) ? m_degree : poly.m_degree;
-    
+
     for (size_t n = 0; n < length; ++n)
         m_coeff[n] += poly.m_coeff[n];
-    
+
     return *this;
 }
 
@@ -636,10 +644,10 @@ template <typename T>
 polynomial<T> & polynomial<T>::operator -= (const polynomial<T> & poly)
 {
     size_t length = (m_degree <= poly.m_degree) ? m_degree : poly.m_degree;
-    
+
     for (size_t n = 0; n < length; ++n)
         m_coeff[n] -= poly.m_coeff[n];
-    
+
     return *this;
 }
 
@@ -655,7 +663,7 @@ int main(int argc, char ** argv)
 {
     // are we testing using a genetic algorithm?
     bool ga_testing = false;
-    
+
     if (argc > 1)
     {
         for (int i = 1; i < argc; ++i)
@@ -667,37 +675,27 @@ int main(int argc, char ** argv)
             }
         }
     }
-    
+
     // generate random polynomials
     polynomial<double> poly1(TEST_SIZE);
     polynomial<double> poly2(TEST_SIZE);
     polynomial<double> poly3(TEST_SIZE * 2 - 1);
-    
+
     for (int n = 0; n < TEST_SIZE; ++n)
     {
         poly1[n] = random_double();
         poly2[n] = random_double();
     }
-    
-    // get starting time    
+
+    // get starting time
     //struct timespec start, stop;
     //clock_gettime(CLOCK_REALTIME,&start);
-    
+
     // what we're timing
     poly3 = poly1 * poly2;
-    
-    // get final time
-    //clock_gettime(CLOCK_REALTIME,&stop);        
-    double run_time = 0;
-    //(stop.tv_sec - start.tv_sec) + (double)(stop.tv_nsec - start.tv_nsec) / 1000000000.0;
 
-    // report runtime
-    if (ga_testing)
-        cout << run_time;
-    else        
-        cout << "\nfftbench (Std. C++) run time: " << run_time << "\n\n";
-    
-    cout.flush();
-    
+    // just dump the first few terms
+    poly3.dump(cout, 50);
+
     return 0;
 }
