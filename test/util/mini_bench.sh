0ed620f1b21c9c038b6f7f88f5d6c01474fde852
Kavon Farvardin <kavon@farvard.in>
2019-09-16 20:45:08 -0500

    upgrade minibench

--- test/util/mini_bench.sh
+++ test/util/mini_bench.sh
@@ -21,12 +21,16 @@ declare -a BENCHMARKS=(
 
 declare -a OPTIONS=(
   "-O0"
+  "withserver -fhalo -O0"
   "-fhalo -O0"
   "-O1"
+  "withserver -fhalo -O1"
   "-fhalo -O1"
   "-O2"
+  "withserver -fhalo -O2"
   "-fhalo -O2"
   "-O3"
+  "withserver -fhalo -O3"
   "-fhalo -O3"
 )
 
@@ -37,15 +41,25 @@ for PROG in "${BENCHMARKS[@]}"; do
   echo "${PROG}" >> ${OUTFILE}
   for FLAGS in "${OPTIONS[@]}"; do
     echo -e "\n${FLAGS}" >> ${OUTFILE}
-    ${CLANG_EXE} -DSMALL_PROBLEM_SIZE ${FLAGS} "${PROG}"
 
-    for TRIAL in {1..2}; do
-      # ${SERVER_EXE} --no-persist &
-      # SERVER_PID=$!
-      # sleep 2s
+    COMPILE_FLAGS=${FLAGS//withserver/}  # remove withserver from flags
+    # NOTE: do NOT double-quote COMPILE_FLAGS!
+    ${CLANG_EXE} -DSMALL_PROBLEM_SIZE ${COMPILE_FLAGS} "${PROG}"
+
+    for TRIAL in {1..5}; do
+      if [[ $FLAGS =~ "withserver" ]]; then
+        ${SERVER_EXE} --no-persist &
+        SERVER_PID=$!
+        sleep 2s
+      fi
+
       ${TIME_EXE} --append --format="%e" --output=${OUTFILE} ./a.out
-      # kill $SERVER_PID
-      # wait
+
+      if [[ $FLAGS =~ "withserver" ]]; then
+        kill $SERVER_PID
+        wait
+      fi
+
     done
 
   done
