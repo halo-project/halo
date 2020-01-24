0e4a5a2a70d2f0ef7119cbf43c54016f11e076f5
Kavon Farvardin <kavon@farvard.in>
2020-01-24 15:51:10 -0600

    workaround and note about issue #23

--- test/basic/noop.cpp
+++ test/basic/noop.cpp
@@ -5,11 +5,23 @@
 // RUN: %testhalo %server 1 %t
 
 // RUN: %clang -fhalo %s -o %t
-// RUN: %testhalo %server 50 %t
+// RUN: %testhalo %server 7 %t
 
 // RUN: %clang -fhalo -fpic -fpie %s -o %t
-// RUN: %testhalo %server 50 %t
+// RUN: %testhalo %server 7 %t
 
+
+
+//////
+// when compiled with -O0,
+// this should end up wasting time in a function
+// that is not patchable, in order to test such
+// a situation where there is nothing the server can do.
 int main() {
-  return 0;
+  unsigned i = ~0;
+
+  while (i > 1)
+    i--;
+
+  return i-1;
 }
