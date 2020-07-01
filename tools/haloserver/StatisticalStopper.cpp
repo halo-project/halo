78481f3b79b0c343e2d5b60dd50caed52e827330
Kavon Farvardin <kavon@farvard.in>
2020-07-01 12:57:08 -0500

    stop if low probability of finding a unique compile

--- tools/haloserver/StatisticalStopper.cpp
+++ tools/haloserver/StatisticalStopper.cpp
@@ -5,6 +5,10 @@ namespace halo {
 
 using VersionMap = std::unordered_map<std::string, CodeVersion>;
 
+// TODO: this doesn't actually implement Vuduc et al's decision procedure.
+// The reason is that the space is so huge that even 1% of it would take
+// a very long time to check. All of the spaces checked in the paper
+// were on the order of 5% or higher.
 bool StatisticalStopper::shouldStop(std::string const& BestLib,
                                     VersionMap const& Versions,
                                     ConfigManager const& Manager) const {
@@ -21,18 +25,20 @@ bool StatisticalStopper::shouldStop(std::string const& BestLib,
   double TotalUnique = (N * UniqueConfigs) / ConfigsCompiled;
 
   double NumQualityEstimates = Manager.size();
+  double UniqueCompileProbability = (UniqueConfigs / ConfigsCompiled);
 
   clogs() << "shouldStop:"
           << "\n\ttotal space sz = " << N
           << "\n\tconfigs compiled = " << ConfigsCompiled
           << "\n\tunique configs = " << UniqueConfigs
+          << "\n\tunique compile probability = " << UniqueCompileProbability
           << "\n\ttotal unique sz = " << TotalUnique
           << "\n\tpct of unique checked = " << 100.0 * (UniqueConfigs / TotalUnique) << "%"
           << "\n\tpct total config checked = " << 100.0 * (NumQualityEstimates / N) << "%"
           << "\n";
 
-  // TODO: implement this!
-  return false;
+  double minimumSuccessProb = 0.01; // 1%
+  return UniqueCompileProbability < minimumSuccessProb;
 }
 
 } // namespace halo
